8fe917d88d03eb764f2f81edd898f980cc3386ed
josedanielchg <j.daniel.chg@gmail.com>
2022-08-01 02:04:17 -0400

    Add CRUD to rutas

--- admin/rutas.php
+++ admin/rutas.php
@@ -11,108 +11,94 @@
     if (strlen($_SESSION['tmsc_id'] == 0)):
         header('location:logout.php');
     else:
+        $res = mysqli_query($con,"SELECT r.id, c.name AS company, r.cost, r.created_at FROM rutes AS r JOIN companies AS c ON r.company_id = c.id ORDER BY r.created_at DESC");
+
+        // If user wants to delete a user
+        if($_GET['del']){
+            $rute_id = $_GET['id'];
+            $query = "DELETE from rutes where id =$rute_id";
+            mysqli_query($con, $query);
+            echo "<script>alert('Ruta Eliminada')</script>";
+            echo "<script>window.location.href='rutas.php'</script>";
+        }
 ?>
 
 <?php include_once('includes/header.php');?>
 
-div>
+<div>
 <div class="admin_background"></div>
-<div class="container admin_form_container">
-    <div class="row" >
-        <!-- BASIC FORM START -->
+<div class="container">
+    
+    <div class="row">
+
+        <!-- DATA TABLE START -->
         <div class="col-12 mt-5">
+            
+            <!-- CARD START -->
             <div class="card">
                 <div class="card-body">
-                    <!-- title -->
-                    <figure class="text-center">
-                        <blockquote class="blockquote">
-                            <h4 class="header-title">Crear un Usuario</h4>
-                        </blockquote>
-                    </figure>
-                    <!-- title end -->
+                    <h4 class="header-title">Manejador de Rutas</h4>
+                    <a href="crear-ruta.php" class="btn btn-primary btn-m">Create</a>
                     
-                    <form action="#" method="POST" name="create_user" class="row g-3 form-create_user">
+                    <div class="data-tables">
+                        <table class="table text-center">
+                            
+                            <!-- TABLE HEAD -->
+                            <thead class="bg-light text-capitalize">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Compañia</th>
+                                    <th>Costo</th>
+                                    <th>Paradas</th>
+                                    <th>Fecha de Creación</th>
+                                    <th>Acción</th>
+                                </tr>
+                            </thead>
+                            <!-- TABLE END HEAD -->
 
-                        <!-- Nombre Empresa -->
-                        <div class="form-group col-md-6">
-                            <label for="name">Nombre de Empresa:</label>
-                            <input type="text"
-                                class="form-control" 
-                                id="name" 
-                                name="name" 
-                                placeholder="Ingrese nombre" 
-                                required="true"
-                                value=" ">
-                        </div>
+                            <!-- TABLE BODY -->
+                            <tbody>
+                                <?php while ( $row = mysqli_fetch_array($res) ): ?>
+                                    <?php
+                                        $paradas_query = mysqli_query($con,"SELECT s.name, s.created_at FROM rutes_stops AS rs JOIN stops AS s ON s.id = rs.stops_id WHERE rutes_id = '{$row['id']}' ORDER BY rs.sequence");
+                                        $has_paradas = NULL;
+                                    ?>
+                                    <tr data-expanded="true">
+                                        <td><?php echo $row['id'];?></td>
+                                        <td><?php echo $row['company'];?></td>
+                                        <td><?php echo $row['cost'];?> COP</td>
+                                        <td>
+                                            <!-- Paradas -->
+                                            <ol>
+                                                <?php while ( $paradas = mysqli_fetch_array($paradas_query) ): ?>
+                                                    <?php $has_paradas = true; ?>
+                                                    <li><?php echo $paradas['name']; ?></li>
+                                                <?php endwhile; ?>
 
-                        <!-- Punto de Inicio -->
-                        <div class="form-group col-md-6">
-                            <label for="punto_inicio">Punto de Inicio:</label>
-                            <input type="text" 
-                                class="form-control" 
-                                id="punto_inicio" 
-                                name="punto_inicio" 
-                                placeholder="Ingrese Punto de Inicio" 
-                                required="true"
-                                value=" ">
-                        </div>
-                        
-                        <!-- Punto Final -->
-                        <div class="form-group">
-                            <label for="punto_final">Punto Final:</label>
-                            <input type="text" 
-                                class="form-control" 
-                                id="punto_final" 
-                                name="punto_final" 
-                                placeholder="Ingrese Punto Final" 
-                                required="true"
-                                value=" "> 
-                        </div>
+                                                <?php if( is_null($has_paradas) ): ?>
+                                                    <strong>Paradas no agregadas</strong>
+                                                <?php endif; ?>
+                                            </ol>
+                                        </td>
+                                        <td><?php echo $row['creation_date'] ?? "2022-05-04";?></td>
+                                        <td>
+                                            <a href="editar-rutas.php?edit_id=<?php echo $row['id'];?>" class="btn btn-primary btn-xs">Edit</a>
+                                            <a href="rutas.php?id=<?php echo $row['id']?>&del=delete" onClick="return confirm('Are you sure you want to delete?')" class="btn btn-danger btn-xs">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php endwhile; ?>
+                            </tbody>
+                            <!-- TABLE END BODY -->
 
-                        <!-- Costo -->
-                        <div class="form-group">
-                            <label for="costo">Costo:</label>
-                            <input type="text" 
-                                class="form-control" 
-                                id="costo" 
-                                name="costo" 
-                                placeholder="Ingrese Costo" 
-                                required="true"
-                                value=" "> 
-                        </div>
-
-                        <!-- Puntos Intermedios -->
-                        <div class="form-group">
-                            <label for="puntos_intermedios" class="form-label">Puntos Intermedios:</label>
-                            <select id="puntos_intermedios" class="form-select">
-                                <option selected>1234...</option>
-                                <option>...2455</option>
-                            </select>
-                        </div>
-
-                        <!-- Agregar Parada -->
-                        <div class="form-group col-md-9">
-                            <label for="agregar_parada"> Agregar Parada:</label>
-                            <input type="text" 
-                                class="form-control" 
-                                id="agregar_parada" 
-                                required="true"
-                                value=" "> 
-                        </div>
-                        <div class="form-group col-md-2 search-form__submit-bttn">
-                            <button class="btn mt-4 pr-4 pl-4" >Agregar</button>
-                        </div>
-
-
-                        <div class="search-form__submit-bttn">
-                            <button type="submit" value="Enviar" class="btn mt-4 pr-4 pl-4" name="create_user">Buscar</button>
-                        </div>
-                    </form>
+                        </table>
+                    </div>
                 </div>
             </div>
+            <!-- CARD END -->
+
         </div>
-        <!-- basic form end -->
-        
+        <!-- DATA TABLE END -->
+
     </div>
 </div>
 </div>
